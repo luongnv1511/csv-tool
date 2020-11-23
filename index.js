ef8283db9d87da10d7559f11beb8d5d20b3261e5
LUONGNV <luongnv@LUONG-2.local>
2020-11-23 09:49:29 +0700

    fix ok

--- index.js
+++ index.js
@@ -1,48 +1,60 @@
+const csv = require("csv-parser");
 const fs = require("fs");
-const parse = require("csv-parse");
-const stringify = require("csv-stringify");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const results = [];
 
 const args = process.argv.slice(2);
-const fileSrc = args[0];
-const fileExport = args[1];
+const fileInput = args[0];
+const fileOutput = args[1];
 const brand = args[2];
-let count = 1;
 
-fs.createReadStream(__dirname + `/${fileSrc}`)
-  .pipe(parse({ delimiter: "\t", quote: '"', skip_empty_lines: true }))
-  .on("data", async (csvRow) => {
-    const row0 = csvRow[0];
-    if (row0 && row0 == "id") {
-      await csvRow.unshift("mpn");
-      await csvRow.unshift("chanel");
-      await csvRow.unshift("content_language");
-      await csvRow.unshift("target_country");
-      await csvRow.unshift("offer_id");
-    } else {
-      await csvRow.unshift(row0);
-      await csvRow.unshift("online");
-      await csvRow.unshift("en");
-      await csvRow.unshift("US");
-      await csvRow.unshift(row0);
+const csvWriter = createCsvWriter({
+  path: __dirname + `/${fileOutput}`,
+  header: [
+    { id: "id", title: "id" },
+    { id: "title", title: "title" },
+    { id: "brand", title: "brand" },
+    { id: "link", title: "link" },
+    { id: "price", title: "price" },
+    { id: "description", title: "description" },
+    { id: "image_link", title: "image_link" },
+    { id: "mpn", title: "mpn" },
+    { id: "gtin", title: "gtin" },
+    { id: "availability", title: "availability" },
+    { id: "condition", title: "condition" },
+    { id: "product_type", title: "product_type" },
+    { id: "product_category", title: "product_category" },
+    { id: "bingads_grouping", title: "bingads_grouping" },
+    { id: "bingads_label", title: "bingads_label" },
+    { id: "custom_label_0", title: "custom_label_0" },
+    { id: "custom_label_1", title: "custom_label_1" },
+    { id: "custom_label_2", title: "custom_label_2" },
+    { id: "custom_label_3", title: "custom_label_3" },
+    { id: "custom_label_4", title: "custom_label_4" },
+    { id: "bingads_redirect", title: "bingads_redirect" },
+    { id: "sale_price", title: "sale_price" },
+    { id: "sale_price_effective_date", title: "sale_price_effective_date" },
+  ],
+});
+
+fs.createReadStream(__dirname + `/${fileInput}`)
+  .pipe(csv({ separator: "\t" }))
+  .on("data", (data) => {
+    if (data.id) {
+      data.mpn = data.id;
+      if (!data.price) return;
+      data.price = Number(data.price.match(/\d*.\d*/)[0]).toFixed(2);
+      if (data.sale_price) {
+        data.sale_price = Number(data.sale_price.match(/\d*.\d*/)[0]).toFixed(
+          2
+        );
+      }
+      data.brand = brand;
+      results.push(data);
     }
-    if (csvRow[12] != "price") {
-      const price = {
-        currency: "USD",
-        value: Number(csvRow[12].match(/\d*.\d*/)[0]).toFixed(2),
-      };
-      csvRow[12] = JSON.stringify(price);
-    }
-    if (csvRow[14] != "brand") {
-      csvRow[14] = brand;
-    }
-    stringify([csvRow], (err, output) => {
-      fs.appendFile(fileExport, output, "utf8", (err) => {
-        if (err) {
-          console.log("err: ", err);
-        } else {
-          console.log("Row Number: ", count);
-          count += 1;
-        }
-      });
-    });
+  })
+  .on("end", () => {
+    csvWriter
+      .writeRecords(results)
+      .then(() => console.log("The CSV file was written successfully"));
   });
